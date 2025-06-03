9fc87a6076af33a4c43c2d9e00f2e5f93c50eada
Jose Perera <70457016+josevictorperera@users.noreply.github.com>
2025-06-03 12:18:05 -0500

    Update scripts.js
    add floater

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,20 @@ document.addEventListener("DOMContentLoaded", () => {
     form.reset();
   });
 });
+
+// Place floater
+
+document.addEventListener("DOMContentLoaded", function () {
+  const infoBox = document.getElementById("infoFija");
+  const toggleBtn = document.getElementById("toggleInfoBtn");
+
+  toggleBtn.addEventListener("click", function () {
+    if (infoBox.classList.contains("d-none")) {
+      infoBox.classList.remove("d-none");
+      toggleBtn.textContent = "Ocultar información";
+    } else {
+      infoBox.classList.add("d-none");
+      toggleBtn.textContent = "Mostrar información";
+    }
+  });
+});
