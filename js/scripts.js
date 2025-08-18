0df7f30b86a2bb8a8bbcb37c7e58032ff3836d90
Jose Perera <70457016+josevictorperera@users.noreply.github.com>
2025-08-18 15:21:06 -0600

    Update scripts.js
    removed form code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Placeholder for future JS functionality
 console.log("Página cargada: Clínica Nuestra Señora de Fátima");
 // Mostrar mensaje cuando se envía el formulario
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Gracias por contactarnos. Le responderemos pronto.");
-    form.reset();
-  });
-});
+//document.addEventListener("DOMContentLoaded", () => {
+//  const form = document.querySelector("form");
+ // form.addEventListener("submit", function (e) {
+  //  e.preventDefault();
+ //   alert("Gracias por contactarnos. Le responderemos pronto.");
+//    form.reset();
+//  });
+//});
 
 // Place floater
 
